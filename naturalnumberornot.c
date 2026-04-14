7c78a410adbffa3dce9ff7e8c1c75a13f180779c
jaswant choudhary <choudharyjaswant070@gmail.com>
2026-04-14 22:21:29 +0530

    Add files via upload

---
+++ naturalnumberornot.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int num;
+    printf("enter number : ");
+    scanf("%d", & num);
+    if(num >=1 ) {
+        printf("natural number");
+    } 
+    else {
+        printf("not natural number");
+    }
+return 0;
+}
